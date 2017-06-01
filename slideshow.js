23efd62e94e2e1e8ccb89be1d82ac8b07b9e6e00
Viktor Ahlund <id14vad@cs.umu.se>
2017-06-01 16:36:57 +0200

    Första

---
+++ slideshow.js
@@ -0,0 +1,10 @@
+$("#home > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#home');
+},  3000);
